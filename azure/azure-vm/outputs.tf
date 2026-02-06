edeea07473e1b4f432ca0e2f361ad09e0b353bdf
kamarajugadda <salini2579@gmail.com>
2026-02-06 15:04:53 +0530

    Deploy

--- azure/azure-vm/outputs.tf
+++ azure/azure-vm/outputs.tf
@@ -1,6 +1,7 @@
 output "vm_ip" {
    value       = local.vm_public_ip ? azurerm_linux_virtual_machine.vm_inst[0].public_ip_address : azurerm_linux_virtual_machine.vm_inst_private[0].private_ip_address
    description = "Azure VM IP"
+   sensitive = true
 }
 
 output "arcadia_port" {
