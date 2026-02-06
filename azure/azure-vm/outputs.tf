872a0b20b07501f9e78bd2bc7d0e11108a585bff
kamarajugadda <salini2579@gmail.com>
2026-02-06 15:59:35 +0530

    Deploy

--- azure/azure-vm/outputs.tf
+++ azure/azure-vm/outputs.tf
@@ -1,5 +1,5 @@
 output "vm_ip" {
-   value       = local.vm_public_ip ? azurerm_linux_virtual_machine.vm_inst[0].public_ip_address : azurerm_linux_virtual_machine.vm_inst_private[0].private_ip_address
+   value       = nonsensitive(local.vm_public_ip ? azurerm_linux_virtual_machine.vm_inst[0].public_ip_address : azurerm_linux_virtual_machine.vm_inst_private[0].private_ip_address)
    description = "Azure VM IP"
 }
 
